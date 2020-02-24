be468ef86eb47fe338a35d09c3cd3609941a5e13
Kevin Gutowski <kgutowski@berkeley.edu>
2020-02-23 18:44:03 -0800

    Initial commit

---
+++ src/my-command.js
@@ -0,0 +1,31 @@
+import sketch from 'sketch'
+// documentation: https://developer.sketchapp.com/reference/api/
+
+export default function() {
+  let sketch = require('sketch')
+  let UI = sketch.UI
+  let doc = sketch.getSelectedDocument()
+  let sharedTextStyles = doc.sharedTextStyles
+
+  for (const sharedStyle of sharedTextStyles) {
+    const layers = sharedStyle.getAllInstancesLayers()
+
+    for (const layer of layers) {
+      let isOutOfSync = layer.style.isOutOfSyncWithSharedStyle(sharedStyle)
+
+      if (isOutOfSync) {
+        zoomToLayer(layer)
+        UI.message("💩 Dirty style found");
+        return
+      }
+    }
+  }
+
+  UI.message("✨ Shared styles all clean: No out of sync layers found");
+
+  function zoomToLayer(layer) {
+    doc.selectedLayers = [];
+    layer.selected = true;
+    doc.sketchObject.eventHandlerManager().currentHandler().zoomToSelection()
+  }
+}
