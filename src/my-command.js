55aedf2082b76ebefea2186f6a0f4644b9b6d7a4
Kevin Gutowski <kgutowski@berkeley.edu>
2020-03-07 10:11:17 -0800

    Add layer style support

--- src/my-command.js
+++ src/my-command.js
@@ -2,15 +2,15 @@ import sketch from 'sketch'
 let Settings = sketch.Settings
 // documentation: https://developer.sketchapp.com/reference/api/
 
-export function findDirtyTextStyles() {
-    let UI = sketch.UI
-    let doc = sketch.getSelectedDocument()
-    let sharedTextStyles = doc.sharedTextStyles
+let UI = sketch.UI
+let doc = sketch.getSelectedDocument()
 
+export function findDirtyTextStyles() {
+    let sharedTextStyles = doc.sharedTextStyles
     let firstLayerSkipped
     let previouslyFound
-    if (Settings.sessionVariable('dirtyLayerIDs')) {
-        previouslyFound = Settings.sessionVariable('dirtyLayerIDs')
+    if (Settings.sessionVariable('dirtyTextLayerIDs')) {
+        previouslyFound = Settings.sessionVariable('dirtyTextLayerIDs')
     } else {
         previouslyFound = []
     }
@@ -34,8 +34,63 @@ export function findDirtyTextStyles() {
             let isOutOfSync = layer.style.isOutOfSyncWithSharedStyle(sharedStyle)
             if (isOutOfSync) {
                 previouslyFound.push(layer.id)
+                Settings.setSessionVariable('dirtyTextLayerIDs', previouslyFound)
+                UI.message("💩 Dirty text style found");
+                zoomToLayer(layer)
+                return
+            }
+        }
+    }
+
+    if (firstLayerSkipped) {
+        Settings.setSessionVariable('dirtyTextLayerIDs', [firstLayerSkipped.id])
+        UI.message("💩 Dirty text style found (Back to top)");
+        zoomToLayer(firstLayerSkipped)
+        return
+    } else {
+        UI.message("✨ Shared text styles all clean: No out of sync layers found");
+    }
+
+    function zoomToLayer(layer) {
+        let page = layer.getParentPage()
+        doc.selectedPage = page
+        doc.selectedLayers.clear()
+        layer.selected = true;
+        doc.sketchObject.eventHandlerManager().currentHandler().zoomToSelection()
+    }
+}
+
+export function findDirtyLayerStyles() {
+    let sharedLayerStyles = doc.sharedLayerStyles
+    let firstLayerSkipped
+    let previouslyFound
+    if (Settings.sessionVariable('dirtyLayerIDs')) {
+        previouslyFound = Settings.sessionVariable('dirtyLayerIDs')
+    } else {
+        previouslyFound = []
+    }
+
+    for (const sharedStyle of sharedLayerStyles) {
+        let layers = sharedStyle.getAllInstancesLayers()
+
+        if (Settings.settingForKey('limitToCurrentPage')) {
+            layers = layers.filter(layer => layer.getParentPage().id == doc.selectedPage.id)
+        }
+
+        for (const layer of layers) {
+
+            if (previouslyFound.includes(layer.id)) {
+                if (!firstLayerSkipped) {
+                    firstLayerSkipped = layer
+                }
+                continue;
+            }
+
+            let isOutOfSync = layer.style.isOutOfSyncWithSharedStyle(sharedStyle)
+            if (isOutOfSync) {
+                previouslyFound.push(layer.id)
                 Settings.setSessionVariable('dirtyLayerIDs', previouslyFound)
-                UI.message("💩 Dirty style found");
+                UI.message("💩 Dirty layer style found");
                 zoomToLayer(layer)
                 return
             }
@@ -44,11 +99,11 @@ export function findDirtyTextStyles() {
 
     if (firstLayerSkipped) {
         Settings.setSessionVariable('dirtyLayerIDs', [firstLayerSkipped.id])
-        UI.message("💩 Dirty style found (Back to top)");
+        UI.message("💩 Dirty layer style found (Back to top)");
         zoomToLayer(firstLayerSkipped)
         return
     } else {
-        UI.message("✨ Shared text styles all clean: No out of sync layers found");
+        UI.message("✨ Shared layer styles all clean: No out of sync layers found");
     }
 
     function zoomToLayer(layer) {
